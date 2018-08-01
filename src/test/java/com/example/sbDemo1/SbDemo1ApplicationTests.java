33d7898c643442affadec4a9ec65d396fe02c53e
顺子 <cenjiashun@zhuojianchina.com>
2018-08-01 08:56:42 +0800

    2018.7.31

--- src/test/java/com/example/sbDemo1/SbDemo1ApplicationTests.java
+++ src/test/java/com/example/sbDemo1/SbDemo1ApplicationTests.java
@@ -1,16 +1,21 @@
 package com.example.sbDemo1;
 
+import com.example.sbDemo1.mapper.UserMapper;
+import com.example.sbDemo1.model.User;
+import com.sun.glass.ui.Application;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
 public class SbDemo1ApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
+	public void contextLoads() throws Exception{
+}
 
 }
